79b48c586689adf7d64866b21e843d35e95116bf
Simon Mwangi <simonmwangi749@gmail.com>
2025-08-07 20:17:45 +0300

    hero section work outs hehehe..

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,19 @@
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { SplitText } from "gsap/all";
+import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger,SplitText);
 
 
 
 const Hero = () => {
+
+  const videoRef = useRef();
+
+  const isMobile = useMediaQuery({ maxWidth: 767 })
   useGSAP (() => {
     const heroSplit = new SplitText('.title', { type: 'chars,words'});
 
@@ -26,7 +35,41 @@ const Hero = () => {
       ease: 'expo.out',
       stagger: 0.06,
       delay: 1,
+    });
+
+    gsap.timeline({
+      scrollTrigger: {
+        trigger:'#hero',
+        start: 'top top',
+        end: 'bottom top',
+        scrub: true,
+      }
     })
+
+      .to('.right-leaf', { y: 200}, 0)
+      .to('.left-leaf', { y: -200}, 0)
+
+      const startValue = isMobile ? 'top 50%' : 'center 60%';
+      const endValue = isMobile ? '120% top' : 'bottom top';
+
+
+      //video animation
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: '.video',
+          start: startValue,
+          end: endValue,
+          scrub: true,
+          pin: true,
+        }
+      })
+
+      videoRef.current.onloadedmetadata = () => {
+        tl.to(videoRef.current, {
+          currentTime: videoRef.current.duration
+        })
+      }
+
   }, []);
   return (
     <>
@@ -54,14 +97,24 @@ const Hero = () => {
 
           <div className="view-cocktails">
             <p className="subtitle">
-              Every cocktail on our menu is a blend of premium ingridients, creative, 
-              flair, and timeless recipies - designed to delight your senses. 
+              Every cocktail on our menu is a blend of premium ingredients, creative, 
+              flair, and timeless recipes - designed to delight your senses. 
             </p>
             <a href="#cocktails">View Cocktails</a>
           </div>
         </div>
        </div>
     </section>
+
+    <div className="video absolute inset-0">
+      <video
+      ref={videoRef}
+       src="/video/input.mp4"
+       muted
+       playsInline
+       preload="auto"
+      />
+    </div>
     </>
   )
 }
